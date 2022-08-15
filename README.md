ff465736a02e6bf498d698e1a044cf87a05af2ec
Sean Ezell <sean.ezell@ormutual.com>
2022-08-15 16:15:03 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # what2play-games-microservice
 What2Play - Games API
+
+<!-- BEGIN_TF_DOCS -->
+## Requirements
+
+No requirements.
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 4.26.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_api_gateway_base_path_mapping.apigw-bpm](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_base_path_mapping) | resource |
+| [aws_api_gateway_deployment.apigw-deployment](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_deployment) | resource |
+| [aws_api_gateway_method_settings.s](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_method_settings) | resource |
+| [aws_api_gateway_rest_api.api](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_rest_api) | resource |
+| [aws_api_gateway_stage.StageSettings](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/api_gateway_stage) | resource |
+| [aws_cloudwatch_log_group.api_cw](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_log_group) | resource |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END_TF_DOCS -->
---
+++ terraform/main.tf
@@ -0,0 +1,52 @@
+terraform {
+    backend "s3" {
+        bucket = "seanezell-terraform-backend"
+        key = "what2play-games/terraform.tfstate"
+        region = "us-west-2"
+        dynamodb_table = "terraform_state"
+    }
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+    name = "GamesAPI"
+	description = "Games API for What2Play"
+}
+
+resource "aws_cloudwatch_log_group" "api_cw" {
+    name = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.api.id}/${aws_api_gateway_stage.StageSettings.stage_name}"
+    retention_in_days = 14
+}
+
+resource "aws_api_gateway_deployment" "apigw-deployment" {
+	rest_api_id = aws_api_gateway_rest_api.api.id
+	description = "Deployed on ${timestamp()}"
+
+	lifecycle {
+		create_before_destroy = true
+	}
+}
+
+resource "aws_api_gateway_stage" "StageSettings" {
+	rest_api_id   = aws_api_gateway_rest_api.api.id
+	deployment_id = aws_api_gateway_deployment.apigw-deployment.id
+	stage_name    = "v1"
+}
+
+resource "aws_api_gateway_method_settings" "s" {
+	rest_api_id = aws_api_gateway_rest_api.api.id
+	stage_name  = aws_api_gateway_stage.StageSettings.stage_name
+	method_path = "*/*"
+
+	settings {
+		metrics_enabled = true
+		logging_level   = "INFO"
+		data_trace_enabled = true
+	}
+}
+
+resource "aws_api_gateway_base_path_mapping" "apigw-bpm" {
+	api_id      = aws_api_gateway_rest_api.api.id
+	domain_name = "api.seanezell.com"
+	base_path = "games"
+	stage_name = aws_api_gateway_stage.StageSettings.stage_name
+}
